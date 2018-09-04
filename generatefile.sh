b273ecd1a19a70704dad9c2da5689e00ff2940eb
Angg <angelagilhotra@gmail.com>
2018-09-04 14:14:16 +0530

    changed maxdepth to 1 for find

--- generatefile.sh
+++ generatefile.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-TARGET='/home/angg/code/mca209lab'
+target='/home/angg/code/mca209lab'
 
-cd $TARGET
+cd $target
 
-find . -type f -name '*.cpp' | while read CPPFILE
+find . -maxdepth 1 -type f -name '*.cpp' | while read CPPFILE
 do
     TITLE=$(basename $CPPFILE .cpp)
     g++ $TITLE.cpp 
-    echo $CPPFILE | xargs enscript --color=1 -C -Ecpp -B -t $TITLE -o - | ps2pdf - $TITLE.pdf
+    enscript $CPPFILE --color=1 -C -Ecpp -B -t $TITLE -o - | ps2pdf - $TITLE.pdf
     ./a.out > $TITLE.txt && enscript -B $TITLE.txt -o - | ps2pdf - $TITLE.output.pdf
-    pdftk $TITLE.pdf $TITLE.output.pdf cat output $TITLE.ok.pdf
+    pdftk $TITLE.pdf $TITLE.output.pdf cat output $TITLE.final.pdf
     rm $TITLE.output.pdf
     rm $TITLE.pdf
 done
 
-today=`date +%j-%M`
+today=`date +%j-%H-%M`
 mkdir $today
 
 pdftk *.pdf cat output $today.pdf
@@ -26,5 +26,4 @@ mv *.cpp ./$today
 rm *.txt 
 
 cd './file'
-rm *.ok.pdf
-
+rm *.final.pdf
